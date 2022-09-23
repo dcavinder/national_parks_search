38979b5d51322f1768c7d25820325994457ce169
rdahltorp <109421585+rdahltorp@users.noreply.github.com>
2022-09-22 21:35:07 -0600

    Added in API call for the parks page to get basic parks info, code to set what was searched to local storeage, and working on recalling locally stored code.

--- homepage.js
+++ homepage.js
@@ -1,19 +1,49 @@
 /*Psuedo code - user functionality
 - User types in box
-– If local storeage, create drop down options
 – User clicks search button or hits return to search
 – Seach needs to: 
 –––1) Link to next page
 –––2) Call APIs using search location
+– Store search in local storage
+– Other stuff we could do 
+–––Add in an area where they can add their name/info that we can store and have a welcome message of "Happy trails [NAME]!"
 */
 
 //Global vars
+var searchBtn = document.querySelector() //Need to link to the class/ID of button
+var userSearch = document.getElementById().trim() //Need to link to the input id
 
 
+//Function for form + call API
+function parkSearch() {
+    console.log(userSearch.value); //test to make sure we are getting the specific user earch value
 
-//Function for form
+    var requestParksInfo = 'https://developer.nps.gov/api/v1/parks?api_key=bHs0Q9w8lnRTDFP2HjYarQQNliJq6mm7aFKTeF54' //basic test url. Need to figure out way to insert the user search. 
 
+    fetch(requestParksInfo) 
+    .then(function (response) {
+        return response.json();
+    })    
+    .then(function (data){
+        console.log(data)
+    })   
+    
+    window.location.href = "./results.html"
+    
+}
 
+//Store past searches
+localStorage.setItem("pastSearch", userSearch.val())
 
-//API call 
+//Call past searches from LS
+function int(){
+    stored = JSON.parse(localStorage.getItem("pastSearch"))
+    console.log(stored);
+    for (var i = 0; i < stored.length; i++) {
+        //NEED TO ADD IN HOW TO APPEND PAST SEARCHES TO THE SEACHBAR
+    }
+}
+
+//Button click
+searchBtn.addEventListener("click", parkSearch)
 
