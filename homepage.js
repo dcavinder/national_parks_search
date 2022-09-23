bf41a24b60c3073a70e000d6c618a2452fcfcfb7
rdahltorp <109421585+rdahltorp@users.noreply.github.com>
2022-09-22 19:38:24 -0600

    addition of homepage.js file and psuedo code of user functionality.

---
+++ homepage.js
@@ -0,0 +1,19 @@
+/*Psuedo code - user functionality
+- User types in box
+– If local storeage, create drop down options
+– User clicks search button or hits return to search
+– Seach needs to: 
+–––1) Link to next page
+–––2) Call APIs using search location
+*/
+
+//Global vars
+
+
+
+//Function for form
+
+
+
+//API call 
+
